d696b311e6a0da41ca99f7d73c56371fc2f6526c
v4dmz <143401420+v4dmz@users.noreply.github.com>
2024-03-17 05:07:19 -0700

    JavaZiqCart

---
+++ app.js
@@ -0,0 +1,32 @@
+// let welcomeMessage = 'Hello Friends';
+// console.log ('Hello Friends')
+// console.log(welcomeMessage);
+
+// let bagValue = (15599-(10/100 * 15599))- 272
+// let jeansPrice = (2499 - (41/100*2499)) -29
+// let BgValue = Math.round ((jeansPrice + bagValue + 25 + 20 ) * 118 / 100 );
+ 
+
+// let bagSummary = `Your Final Daraz Amount is ${bagValue}`
+
+// console.log (bagSummary);
+
+
+
+// let a =5;
+// let b =6;
+// let c =a + 4;
+// console.log(c);
+// c =b +2;
+// console.log(c);
+
+// let i =5;
+// console.log(i);
+// // i = i + 5;
+// i = i +5;
+// console.log(i);
+
+
+let cartQuantity = 0;
+document.querySelector('#cart-summary').
+innerText = `Your Bag has ${cartQuantity} Items`
